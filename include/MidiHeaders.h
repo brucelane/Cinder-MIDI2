4c927a351af9f2011c522ee4c8acca0f4b5461a6
MartinBspheroid <mblasko42@gmail.com>
2014-03-23 11:22:57 +0100

    Initial commit

---
+++ include/MidiHeaders.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include <string>
+#include <exception>
+#include "MidiConstants.h"
+#include "MidiExceptions.h"
+#include "MidiMessage.h"
+#include "RtMidi.h"
+#include "MidiIn.h"
+#include "MidiOut.h"
+#include "MidiHub.h"

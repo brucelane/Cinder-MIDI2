de26d5e5b37120e606f28c8b47d6650de3931d8f
BruceLane <bruce@batchass.fr>
2020-06-06 01:48:41 +0200

    mouseDrag x y OSC

--- samples/Midi2Osc/src/Midi2OscApp.cpp
+++ samples/Midi2Osc/src/Midi2OscApp.cpp
@@ -57,6 +57,10 @@ public:
 	void setup() override;
 	void update() override;
 	void draw() override;
+	void mouseMove(MouseEvent event) override;
+	void mouseDown(MouseEvent event) override;
+	void mouseDrag(MouseEvent event) override;
+	void mouseUp(MouseEvent event) override;
 	void midiListener(midi::Message msg);
 
 	midi::Input mMidiIn;
@@ -181,7 +185,33 @@ void Midi2OscApp::midiListener(midi::Message msg) {
 void Midi2OscApp::update() {
 
 }
+void Midi2OscApp::mouseMove(MouseEvent event)
+{
+}
 
+void Midi2OscApp::mouseDown(MouseEvent event)
+{
+}
+
+void Midi2OscApp::mouseDrag(MouseEvent event)
+{
+	osc::Message oscMx("/cc");
+	oscMx.append(42);
+	oscMx.append((float)event.getX());
+	mSender.send(oscMx, std::bind(&Midi2OscApp::onSendError,
+		this, std::placeholders::_1));
+
+	osc::Message oscMy("/cc");
+	oscMy.append(43);
+	oscMy.append((float)event.getY());
+	mSender.send(oscMy, std::bind(&Midi2OscApp::onSendError,
+		this, std::placeholders::_1));
+	
+}
+
+void Midi2OscApp::mouseUp(MouseEvent event)
+{
+}
 void Midi2OscApp::draw() {
 	gl::clear(Color(0, 0, 0), true);
 	gl::color(Color(1, 1, 1));
